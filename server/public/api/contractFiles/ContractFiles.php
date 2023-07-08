cfccf081023f3a36704de9e712c3d948ee5537ce
Gecko Web <contact@gecko-web.fr>
2023-07-08 18:56:01 +0200

    Bug fix in ContractFiles.php file

--- server/public/api/contractFiles/ContractFiles.php
+++ server/public/api/contractFiles/ContractFiles.php
@@ -3,7 +3,7 @@
 use JBZoo\Utils\Arr;
 use SmartDownloadHelper\ContractTable;
 
-require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/../../../bootstrap.php';
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf-8');
